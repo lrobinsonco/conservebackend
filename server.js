0e091911a27214b2df431f6c8c4c4e026e9dd7af
lrobinsonco <lrobinsonco@gmail.com>
2017-07-14 15:35:05 -0600

    added monk middleware

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ var express =require('express');
 var mongodb = require('mongodb')
 var bodyParser = require('body-parser');
 var cors = require('cors')
+var monk = require('monk')
 
 var port = process.env.PORT || 8080;
 
 var app = express();
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+
 
 // mongodb://heroku_7kjfrlfk:g64o6j6cfuua16gfvi2q5mu8vg@ds157702.mlab.com:57702/heroku_7kjfrlfk
 const mongoURI = process.env.MONGODB_URI;
