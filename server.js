9d61c5eebbff59d28e4a67bc298b1cea07e8a1a9
lrobinsonco <lrobinsonco@gmail.com>
2017-07-16 19:22:12 -0600

    added process env to app.listen

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 // mongodb://heroku_7kjfrlfk:g64o6j6cfuua16gfvi2q5mu8vg@ds157702.mlab.com:57702/heroku_7kjfrlfk
 const mongoURI = process.env.MONGODB_URI;
 
@@ -100,6 +100,6 @@ mongodb.MongoClient.connect(dbUrl, function(err, db) {
     })
   })
 
-  app.listen(8080, () => console.log('Server on localhost:8080'))
+  app.listen(process.env.PORT || 8080, () => console.log('Server on localhost:8080'))
 
 })
